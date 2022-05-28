1c2156577a2f0322809cdd0152e4b5212cc86421
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-28 12:38:31 +0530

    code to solve special pythagorean

--- SpecialPythagoreanTriplet/specialPythagoreanTriplet.cpp
+++ SpecialPythagoreanTriplet/specialPythagoreanTriplet.cpp
@@ -7,10 +7,24 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.*/
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a = 1, b = 2, c = 3;
+    for (int a = 0; a <= 1000; a++)
+    {
+        for (int b = 0; b <= 1000; b++)
+        {
+            for (int c = 0; c <= 1000; c++)
+            {
+                int calc = (a * a) + (b * b);
+                if ((calc == (c * c)) && ((a + b + c) == 1000))
+                {
+                    cout << (a * b * c) << endl;
+                }
+            }
+        }
+    }
 }
