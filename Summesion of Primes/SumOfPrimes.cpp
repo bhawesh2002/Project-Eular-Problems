72db22e07554061fcdf629fc6942c2860c12cd4e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-28 13:20:46 +0530

    add counnter variable

--- Summesion of Primes/SumOfPrimes.cpp
+++ Summesion of Primes/SumOfPrimes.cpp
@@ -8,5 +8,13 @@ using namespace std;
 
 int main()
 {
-    
+    int counter = 2000000;
+    int prime;
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            
+        }
+    }
 }
