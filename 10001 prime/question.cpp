0518e615a99c266461501671958add548abd9e71
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-28 13:20:27 +0530

    optimise the code

--- 10001 prime/question.cpp
+++ 10001 prime/question.cpp
@@ -8,18 +8,17 @@ using namespace std;
 int main()
 {
     int counter = 0;
-    int prime[10001];
+    int prime;
     for (int num = 2; num <= LONG_MAX; num++)
     {
         for (int factor = 2; factor <= num; factor++)
         {
-            if (num % factor == 0)           //yields numbers that gives remainder 0 when num % factor.ForEx:- 2,3,2,5,2,3,7etc.
+            if (num % factor == 0) // yields numbers that gives remainder 0 when num % factor.ForEx:- 2,3,2,5,2,3,7etc.
             {
-                int check = num / factor;       
-                if (check == 1)              //after division if num/factor is 1 then it is a prime number  
+                int check = num / factor;
+                if (check == 1) // after division if num/factor is 1 then it is a prime number
                 {
-                    prime[counter] = factor;
-                    cout << prime[counter] << endl;
+                    prime = factor;
                     counter++;
                 }
                 break;
@@ -27,6 +26,7 @@ int main()
         }
         if (counter == 10001)
         {
+            cout << prime << endl;
             break;
         }
     }
