66ae4f42d71ff504aac9da908c2c9b8e0a2c5024
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-29 13:28:35 +0530

    code to display triangular number

--- triangular numbers/triangular numbers.cpp
+++ triangular numbers/triangular numbers.cpp
@@ -4,5 +4,29 @@ using namespace std;
 
 int main()
 {
+    int counter = 0;
+    int sum = 0;
+    for (int i = 1; i <= LONG_MAX; i++)
+    {
+        sum += i;
+        for (int j = 1; j < sum; j++)
+        {
+            if (sum % j == 0)
+            {
+                counter++;
+            }
+        }
+        cout << "Total : " << counter << endl;
+        if (counter > 500)
+        {
+            cout << sum << endl;
+            break;
+        }
+        else
+        {
+            counter = 0;
+        }
+    }
+
     return 0;
 }
