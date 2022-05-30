2bb5464b8183982e4c69b0ebcd90adee1a099a05
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-30 19:36:34 +0530

    it is not possible to strore 100! even in longlong

--- Factorial Digit  Sum/FactorialDigitSum.cpp
+++ Factorial Digit  Sum/FactorialDigitSum.cpp
@@ -11,5 +11,13 @@ using namespace std;
 
 int main()
 {
+    int res[200];
+    int result = 1;
+    for (int i = 1; i <= 10; i++)
+    {
+        result = result * i;
+    }
+
+    cout << result << endl;
     return 0;
 }
